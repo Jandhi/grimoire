8bed3d813eb383ab44ace5b4d39b365d2e541846
Jan D <jandohring@hotmail.com>
2023-01-19 20:14:30 -0500

    deleted old folder

--- building/structure.py
+++
@@ -1,6 +0,0 @@
-from building.block import Block
-
-class Structure:
-    def __init__(self, blocks : dict[tuple[int, int, int], int], palette : list[Block]) -> None:
-        self.blocks = blocks
-        self.palette = palette
