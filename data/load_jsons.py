86d0bc996f4b2c02425d11231f21be0a2b796b12
Jan D <jandohring@hotmail.com>
2023-01-21 01:01:57 -0500

    loading json objects

--- data/load_jsons.py
+++ data/load_jsons.py
@@ -1,5 +1,6 @@
 import os
 import json
+from types import SimpleNamespace
 
 def load_jsons(directory_path : str) -> list[dict]:
     names = filter(lambda name : name.endswith('.json'), os.listdir(directory_path))
@@ -13,3 +14,16 @@ def load_jsons(directory_path : str) -> list[dict]:
             jsons.append(data)
 
     return jsons
+
+def load_jsons_as_objects(directory_path : str, cls : type) -> list[any]:
+    objects = []
+
+    for json in load_jsons(directory_path):
+        obj : object = cls()
+
+        for key, val in json.items():
+            obj.__setattr__(key, val)
+
+        objects.append(obj)
+    
+    return objects
---
+++ data/tests/test_load_jsons.py
@@ -0,0 +1,15 @@
+# Allows code to be run in root directory
+import sys
+sys.path[0] = sys.path[0].removesuffix('\\data\\tests')
+
+# Actual file
+from data.load_jsons import load_jsons, load_jsons_as_objects
+
+class Test:
+    field : str
+
+print(load_jsons('data/tests'))
+
+test : Test = load_jsons_as_objects('data/tests', Test)[0]
+
+print(test.field)
