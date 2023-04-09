0a847a679b221e6e798d4455dddaf33a750178f4
Jan D <jandohring@hotmail.com>
2023-04-09 17:06:23 -0400

    roads are smooth

--- paths/build_highway.py
+++ paths/build_highway.py
@@ -1,18 +1,19 @@
 from gdpc import Editor, WorldSlice, Block
 from gdpc.vector_tools import ivec3, ivec2
-from structures.directions import cardinal, get_ivec2
+from structures.directions import cardinal, get_ivec2, to_text
 from utils.bounds import is_in_bounds2d
 
 def build_highway(points : list[ivec3], editor : Editor, world_slice: WorldSlice, water_map : list[list[bool]], building_map : list[list[str]]):
-    master_points    : set[ivec2] = set()
-    neighbour_points : set[ivec2] = set()
-    final_points     : dict[ivec2, int] = {}
+    master_points       : set[ivec2] = set()
+    neighbour_points    : set[ivec2] = set()
+    final_point_heights : dict[ivec2, int] = {}
+    blocks              : dict[ivec2, Block] = {}
 
     for point in points:
         point_2d = ivec2(point.x, point.z)
 
         master_points.add(point_2d)
-        final_points[point_2d] = point.y
+        final_point_heights[point_2d] = point.y
 
         for direction in cardinal:
             neighbour = point_2d + get_ivec2(direction)
@@ -24,33 +25,48 @@ def build_highway(points : list[ivec3], editor : Editor, world_slice: WorldSlice
                 continue
 
             neighbour_points.add(neighbour)
-            final_points[neighbour] = point.y # this is an estimate of height to help the next step
-    
-    if False:
-        # We calculate the neighbours of the original highway to be the average height of its neighbours
-        for point in neighbour_points:
-            total_y = 0
-            total_count = 0
-            
-            for direction in cardinal:
-                neighbour = point + get_ivec2(direction)
+            final_point_heights[neighbour] = point.y # this is an estimate of height to help the next step
 
-                if neighbour not in final_points:
-                    continue
-                
-                total_y += final_points[neighbour]
-                total_count += 1
+    for point in final_point_heights:
+        blocks[point] = get_block(point, final_point_heights) 
 
-            average = (total_y + (total_count - 1)) // total_count # formula rounds up, since we would rather have the block "higher" as a slab
-            final_points[point] = average
-
-    for point in final_points:
+    for point in final_point_heights:
         x, z = point
-        y = final_points[point] - 1
+        y = final_point_heights[point] - 1
 
-        editor.placeBlock((x, y, z), Block('cobblestone'))
+        editor.placeBlock((x, y, z), blocks[point])
 
         if world_slice.heightmaps['MOTION_BLOCKING_NO_LEAVES'][x][z] > y:
             editor.placeBlock((x, y + 1, z), Block('air'))
             editor.placeBlock((x, y + 2, z), Block('air'))
             editor.placeBlock((x, y + 3, z), Block('air'))
+
+def get_block(point : ivec2, final_point_heights : dict[ivec2, int]) -> Block:
+    y_in_dir = {}
+    y = final_point_heights[point]
+
+    for direction in cardinal:
+        dv = get_ivec2(direction)
+
+        if point + dv not in final_point_heights:
+            continue
+
+        if abs(final_point_heights[point + dv] - y) >= 2:
+            continue
+
+        y_in_dir[direction] = final_point_heights[point + dv]
+
+        if point - dv not in final_point_heights:
+            continue
+
+        if final_point_heights[point + dv] == y + 1 and final_point_heights[point - dv] == y - 1:
+            return Block('cobblestone_stairs', {'facing' : to_text(direction)})
+        
+    if all(y_in_dir[direction] <= y for direction in y_in_dir) and any(y_in_dir[direction] < y for direction in y_in_dir):
+        return Block('cobblestone_slab')
+    
+    if all(y_in_dir[direction] < y for direction in y_in_dir):
+        final_point_heights[point] -= 1
+        return get_block(point, final_point_heights)
+    
+    return Block('cobblestone')
