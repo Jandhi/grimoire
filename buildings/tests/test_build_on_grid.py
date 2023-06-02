5a1f32f5d0ffd155b0b056bf084a56a55b35fcb1
zachmanson <68168088+zachmanson@users.noreply.github.com>
2023-06-02 16:38:44 -0600

    insides

--- buildings/tests/test_build_on_grid.py
+++ buildings/tests/test_build_on_grid.py
@@ -1,23 +1,37 @@
 # Allows code to be run in root directory
 import sys
-sys.path[0] = sys.path[0].removesuffix('\\building_generation\\tests')
+sys.path[0] = sys.path[0].removesuffix('\\buildings\\tests')
+
+import numpy as np
+
+SEED = 1293786
 
 # Actual file
 from gdpc.editor import Editor
 from structures.grid import Grid
 from buildings.walls.wall import Wall
-from buildings.roofs.roof import Roof
+#from buildings.roofs.roof import Roof
 from buildings.rooms.room import Room
 
+
 from data.load_assets import load_assets
-from structures.directions import cardinal
+from structures.directions import cardinal, get_ivec3, opposite, right, up
 
 from style.style import Style
 from palette.palette import Palette
 
-editor = Editor(transformLike=(0, -61, 0), buffering=True, caching=True)
+editor = Editor(buffering=True, caching=True)
+
+from noise.rng import RNG
+
+from gdpc.vector_tools import ivec3
+
+area = editor.getBuildArea()
+editor.transform = (area.begin.x, 3, area.begin.z)
 grid = Grid()
 load_assets('assets')
+
+rng = RNG(SEED, 'get_origins')
 
 styles = {
     'japanese' : {
@@ -40,14 +54,268 @@ palette : Palette = Palette.find('japanese_palette')
 lower_wall : Wall = Wall.find(style['lower'])
 upper_wall : Wall = Wall.find(style['upper'])
 
-for direction in cardinal:
-    grid.build(editor, lower_wall, palette, (0, 0, 0), direction)
-    grid.build(editor, upper_wall, palette, (0, 1, 0), direction)
+
+XRANGE = 2
+YRANGE = 1
+ZRANGE = 2
+cells_to_fill = [ivec3(x, y, z) for x in range(XRANGE) for y in range(YRANGE) for z in range(ZRANGE) ]
+
+for cell in cells_to_fill:
+    for direction in cardinal:
+            neighbor = cell + get_ivec3(direction)     
+            if neighbor not in cells_to_fill:      
+                grid.build(editor, lower_wall, palette, cell, direction)
 
 # ROOF
-roof : Roof = Roof.find(style['roof'])
+# roof : Roof = Roof.find(style['roof'])
 # roof.build(editor, palette, grid, (0, 2, 0))
 
-# ROOM
-room : Room = Room.find('kitchen_no_window_small')
-grid.build(editor, room, palette, (0, 0, 0))
+
+ROOM_LIST = [
+     'kitchen_no_window_small',
+     'kitchen_corner_1',
+     'bedroom1',
+     'bedroom_centered',
+     'bedroom_corner',
+     'hallway_1'
+]
+
+CONNECTION_LIST = [
+    'wall',
+    'open'
+]
+
+LOWER_STAIRCASE_LIST = [
+    'staircase_corner_lower'
+]
+
+UPPER_STAIRCASE_LIST = [
+    'staircase_corner_upper'
+]
+
+ONEBYONE_UPPER_LIST = [
+    '1x1_twostorey_upper_1'
+]
+
+ONEBYONE_LOWER_LIST = [
+    '1x1_twostorey_lower_1'
+]
+
+ONEBYONE_LIST = [
+    '1x1_room_1'
+]
+
+
+def rotate(a,n=1):
+    if len(a) == 0:
+        return a
+    n = -n % len(a)     # flip rotation direction
+    return np.concatenate((a[n:],a[:n]))  
+
+def is_corner(cell_to_check):
+    corner_combinations = [
+        ["wall", "wall", "open", "open"],
+        ["open", "wall", "wall", "open"],
+        ["open", "open", "wall", "wall"],
+        ["wall", "open", "open", "wall"]
+    ]
+
+    test = []
+    for direction in cardinal:
+        neighbor = cell_to_check + get_ivec3(direction)
+        if neighbor in cells_to_fill:
+            test.append('open')
+        else:
+            test.append('wall')
+    
+    return test in corner_combinations, test
+
+    
+
+
+
+cells_with_rooms = []
+
+def build_start():
+    start_connections = []
+    start = rng.choose(cells_to_fill)
+    for direction in cardinal:
+        neighbor = start + get_ivec3(direction)
+        if neighbor in cells_to_fill:
+            start_connections.append('open')
+        else:
+            start_connections.append('wall')
+
+    #find a room in that fits in that spot
+    potential_rooms = []
+    for a_room in ROOM_LIST:
+        room : Room = Room.find(a_room)
+        to_face = room.facing
+        for i in range(4):
+            if list(rotate(room.connections, i)) == start_connections:
+                potential_rooms.append((room, to_face, rotate(room.connections, i)))
+
+            if i > 0:
+                to_face = right[to_face]
+
+    room_to_build, room_facing, connections = rng.choose(potential_rooms)
+    cells_with_rooms.append((start, connections))
+    grid.build(editor, room_to_build, palette, start, facing = room_facing)
+
+def build_staircase(level):
+    possible_starts = []
+    for potential_start in cells_to_fill:
+        if is_corner(potential_start)[0] and potential_start + get_ivec3(up) in cells_to_fill and potential_start not in cells_with_rooms and list(potential_start)[1] == level:
+            possible_starts.append(potential_start)
+
+    start = rng.choose(possible_starts)
+    start_connections = is_corner(start)[1]
+
+    potential_rooms = []
+    for a_room in LOWER_STAIRCASE_LIST:
+        room : Room = Room.find(a_room)
+        to_face = room.facing
+        for i in range(4):
+            if list(rotate(room.connections, i)) == start_connections:
+                potential_rooms.append((room, to_face, rotate(room.connections, i)))
+
+            if i > 0:
+                to_face = right[to_face]
+
+    room_to_build, room_facing, connections = rng.choose(potential_rooms)
+
+    cells_with_rooms.append((start, connections))
+    cells_with_rooms.append((start + get_ivec3(up), connections))
+    grid.build(editor, room_to_build, palette, start, facing = room_facing)
+    room_to_build : Room = Room.find('staircase_corner_upper')
+    grid.build(editor, room_to_build, palette, start + get_ivec3(up), facing = room_facing)
+
+
+#populate the other cells with rooms
+
+def get_neighbors(rooms, inside_cells):
+    neighbors = set()
+    for room in rooms:
+        for direction in cardinal:
+            new_cell = room + get_ivec3(direction)
+            if new_cell not in rooms and new_cell in inside_cells:
+                neighbors.add(new_cell)
+    return neighbors
+
+def populate_floor(level):
+    rooms_on_floor = [((x ,y, z), con) for (x, y, z), con in cells_with_rooms if y == level]
+    cells_on_floor = [(x, y, z) for x, y, z in cells_to_fill if y == level]
+    while len(rooms_on_floor) != len(cells_on_floor):
+        candidates = []
+        #get the connections for the new rooms
+        for neighbor in get_neighbors([x for x, y in rooms_on_floor], cells_on_floor):
+            north_con, east_con, south_con, west_con = None, None, None, None
+            for c, conns in rooms_on_floor:
+                for direction in cardinal:
+                    if neighbor + get_ivec3(direction) == c:
+                        if direction == 'z_minus':
+                            north_con = conns[2]
+                        elif direction == 'x_plus':
+                            east_con = conns[3]
+                        elif direction == 'z_plus':
+                            south_con = conns[0]
+                        elif direction == 'x_minus':
+                            west_con = conns[1]
+
+            for direction in cardinal:
+                if (neighbor + get_ivec3(direction)) not in cells_on_floor:
+                    if direction == 'z_minus':
+                        north_con = 'wall'
+                    elif direction == 'x_plus':
+                        east_con = 'wall'
+                    elif direction == 'z_plus':
+                        south_con = 'wall'
+                    elif direction == 'x_minus':
+                        west_con = 'wall'
+
+            new_conns = [north_con, east_con, south_con, west_con]
+
+            #i have definitely found the worst way to do this but I just need it to work for now
+
+            entropy = 0
+            potential_rooms = []
+
+            none_idx = []
+            for idx, val in enumerate(new_conns):
+                if val is None:
+                    none_idx.append(idx)
+            
+            for i0 in CONNECTION_LIST:
+                for i1 in CONNECTION_LIST:
+                    for i2 in CONNECTION_LIST:
+                        for i3 in CONNECTION_LIST:
+                            temp = new_conns
+                            for ni in none_idx:
+                                if ni == 0:
+                                    temp[0] = i0
+                                elif ni == 1:
+                                    temp[1] = i1
+                                elif ni == 2:
+                                    temp[2] = i2
+                                elif ni == 3:
+                                    temp[3] = i3
+
+                            for a_room in ROOM_LIST:
+                                room: Room = Room.find(a_room)
+                                to_face = room.facing
+                                for i in range(4):
+                                    if list(rotate(room.connections, i)) == temp:
+                                        if (room, to_face,  list(rotate(room.connections, i))) not in potential_rooms:
+                                            potential_rooms.append((room, to_face, tuple(rotate(room.connections, i))))
+
+                                    if i > 0:
+                                        to_face = right[to_face]
+
+            entropy = len(potential_rooms)
+            candidates.append((neighbor, entropy, potential_rooms))
+
+
+        min_entropy = min([y for x, y, z in candidates])
+        candidates_with_min_entropy = [(x, y, z) for x, y, z in candidates if y == min_entropy]
+        cell_to_build, ent, room_to_build = candidates_with_min_entropy[rng.randint(len(candidates_with_min_entropy))]
+        room_to_build, room_facing, connections = rng.choose(room_to_build)
+
+        rooms_on_floor.append((cell_to_build, connections))
+        cells_with_rooms.append((cell_to_build, connections))
+        grid.build(editor, room_to_build, palette, cell_to_build, facing = room_facing)
+
+def build_one_by_one(num_levels):
+    if num_levels == 1:
+        cell = cells_to_fill[0]
+        pick = rng.choose(ONEBYONE_LIST)
+        room_to_build : Room = Room.find(pick)
+        grid.build(editor, room_to_build, palette, cell)
+    else:
+        cell = [(x, y, z) for x, y, z in cells_to_fill if y == 0][0]
+        lower_pick = rng.choose(ONEBYONE_LOWER_LIST)
+        upper_pick = rng.choose(ONEBYONE_UPPER_LIST)
+        
+        lower_room : Room = Room.find(lower_pick)
+        upper_room : Room = Room.find(upper_pick)
+
+        grid.build(editor, lower_room, palette, cell)
+        grid.build(editor, upper_room, palette, cell + get_ivec3(up))
+
+
+
+number_of_floors = max([y for (x, y, z) in cells_to_fill]) + 1
+
+
+if len(cells_to_fill) == 1 or len(cells_to_fill) == 2 and number_of_floors == 2:
+    build_one_by_one(number_of_floors)
+else:
+    for level in range(number_of_floors):
+        if number_of_floors == 1:
+            build_start()
+        elif level != number_of_floors - 1:
+            build_staircase(level)
+
+        populate_floor(level)
+
+
+
