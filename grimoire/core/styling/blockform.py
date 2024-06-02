d079e7244e482a77f181aeaf341e9151aeb2b628
Jan D <jandohring@hotmail.com>
2024-06-02 00:13:33 -0400

    using lookup more

--- grimoire/core/styling/blockform.py
+++ grimoire/core/styling/blockform.py
@@ -1,6 +1,7 @@
 from enum import Enum, auto
 
 from gdpc import Block
+from gdpc.lookup import STAIRS, SLABS, WALLS, FENCES, DOORS, TRAPDOORS
 
 
 class BlockForm(Enum):
@@ -15,9 +16,20 @@ class BlockForm(Enum):
 
     @staticmethod
     def get_form(block: Block) -> "BlockForm":
-        for form in BlockForm:
-            if form.name.lower() in block.id:
-                return form
+        if block.id in STAIRS:
+            return BlockForm.STAIRS
+        if block.id in SLABS:
+            return BlockForm.SLAB
+        if block.id in WALLS:
+            return BlockForm.WALL
+        if block.id in FENCES:
+            return BlockForm.FENCE
+        if block.id in DOORS:
+            return BlockForm.DOOR
+        if block.id in TRAPDOORS:
+            return BlockForm.TRAPDOOR
+        if "sign" in block.id:
+            return BlockForm.SIGN
 
         # We assume otherwise it is a whole block
         # This may not necessarily be the case, but it's a *good enough* metric
